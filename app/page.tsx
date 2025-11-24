2762e7c3b8edb4add60f3f3f1c4c1b80e223bcd5
soumadip-dev <soumadipmajila@gmail.com>
2025-11-24 19:10:26 +0530

    feat: introduce main application page with featured event listings and an explore button.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,59 @@
+import EventCard from '@/components/EventCard';
 import ExploreBtn from '@/components/ExploreBtn';
-import React from 'react';
 
-const Pnbbvage = () => {
+const events = [
+  {
+    title: 'Tech Expo 2025',
+    image: '/images/event1.png',
+    slug: 'event-1',
+    location: 'Bangalore',
+    date: '2025-12-10',
+    time: '09:30 AM',
+  },
+  {
+    title: 'Developers Meetup',
+    image: '/images/event2.png',
+    slug: 'event-2',
+    location: 'Hyderabad',
+    date: '2025-12-14',
+    time: '02:00 PM',
+  },
+
+  {
+    title: 'AI Conference',
+    image: '/images/event3.png',
+    slug: 'event-3',
+    location: 'Mumbai',
+    date: '2025-12-20',
+    time: '11:15 AM',
+  },
+  {
+    title: 'Product Launch Event',
+    image: '/images/event4.png',
+    slug: 'event-4',
+    location: 'Delhi',
+    date: '2026-01-05',
+    time: '04:45 PM',
+  },
+  {
+    title: 'Tech Career Fair',
+    image: '/images/event5.png',
+    slug: 'event-5',
+    location: 'Chennai',
+    date: '2026-01-18',
+    time: '10:20 AM',
+  },
+  {
+    title: 'Open Source Summit',
+    image: '/images/event6.png',
+    slug: 'event-6',
+    location: 'Pune',
+    date: '2026-01-30',
+    time: '01:00 PM',
+  },
+];
+
+const Page = () => {
   return (
     <section>
       <h1 className="text-center">
@@ -11,9 +63,11 @@ const Pnbbvage = () => {
       <ExploreBtn />
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
-        <ul className="events">
-          {[1, 2, 3, 4, 5].map(event => (
-            <li key={event}>Event {event}</li>
+        <ul className="events list-none">
+          {events.map(event => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
           ))}
         </ul>
       </div>
@@ -21,4 +75,4 @@ const Pnbbvage = () => {
   );
 };
 
-export default Pnbbvage;
+export default Page;
