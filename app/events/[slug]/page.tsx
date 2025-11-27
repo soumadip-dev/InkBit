32bbc1c2a3ffce684fd4505260c3f570c9bb73e0
soumadip-dev <soumadipmajila@gmail.com>
2025-11-27 22:32:01 +0530

    feat: add dynamic event details page to display individual event information

---
+++ app/events/[slug]/page.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string }) => (
+  <div className="flex-row-gap-2 items-center">
+    <Image src={icon} alt={alt} width={17} height={17} />
+    <p>{label}</p>
+  </div>
+);
+
+const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
+
+  const request = await fetch(`${BASE_URL}/api/events/${slug}`);
+  const { event } = await request.json();
+
+  const {
+    description,
+    image,
+    overview,
+    date,
+    time,
+    location,
+    mode,
+    agenda,
+    audience,
+    tags,
+    organizer,
+  } = event;
+
+  if (!event) {
+    return notFound();
+  }
+
+  return (
+    <section id="event">
+      <div className="header">
+        <h1>Event Description</h1>
+        <p>{description}</p>
+      </div>
+      <div className="details">
+        {/*    Left Side - Event Content */}
+        <div className="content">
+          <Image src={image} alt="Event Banner" width={800} height={800} className="banner" />
+
+          <section className="flex-col-gap-2">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+          </section>
+
+          <section className="flex-col-gap-2">
+            <h2>Event Details</h2>
+
+            <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
+            <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
+            <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
+            <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
+            <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
+          </section>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EventDetailsPage;
