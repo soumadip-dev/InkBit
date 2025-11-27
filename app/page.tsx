180bcadfc643865b8a69ca6ec5b35612d22c3c3d
soumadip-dev <soumadipmajila@gmail.com>
2025-11-27 22:02:35 +0530

    feat: implement homepage displaying featured events and an explore button.

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,12 @@
 import EventCard from '@/components/EventCard';
 import ExploreBtn from '@/components/ExploreBtn';
+import { IEvent } from '@/database';
 
-const events = [
-  {
-    title: 'Tech Expo 2025',
-    image: '/images/event1.png',
-    slug: 'event-1',
-    location: 'Bangalore',
-    date: '2025-12-10',
-    time: '09:30 AM',
-  },
-  {
-    title: 'Developers Meetup',
-    image: '/images/event2.png',
-    slug: 'event-2',
-    location: 'Hyderabad',
-    date: '2025-12-14',
-    time: '02:00 PM',
-  },
 
-  {
-    title: 'AI Conference',
-    image: '/images/event3.png',
-    slug: 'event-3',
-    location: 'Mumbai',
-    date: '2025-12-20',
-    time: '11:15 AM',
-  },
-  {
-    title: 'Product Launch Event',
-    image: '/images/event4.png',
-    slug: 'event-4',
-    location: 'Delhi',
-    date: '2026-01-05',
-    time: '04:45 PM',
-  },
-  {
-    title: 'Tech Career Fair',
-    image: '/images/event5.png',
-    slug: 'event-5',
-    location: 'Chennai',
-    date: '2026-01-18',
-    time: '10:20 AM',
-  },
-  {
-    title: 'Open Source Summit',
-    image: '/images/event6.png',
-    slug: 'event-6',
-    location: 'Pune',
-    date: '2026-01-30',
-    time: '01:00 PM',
-  },
-];
-
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">
@@ -64,11 +17,13 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events list-none">
-          {events.map(event => (
-            <li key={event.title}>
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
