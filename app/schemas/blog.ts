c2410f0258ed8502a7c83a042696dfe3de935f05
soumadip-dev <soumadipmajila@gmail.com>
2026-01-10 13:28:30 +0530

    feat: create UI for blog creation flow

---
+++ app/schemas/blog.ts
@@ -0,0 +1,9 @@
+import z from 'zod';
+
+export const blogSchema = z.object({
+  title: z
+    .string()
+    .min(3, 'Title must be at least 3 characters long')
+    .max(50, 'Title must be at most 50 characters long'),
+  content: z.string().min(10, 'Content must be at least 10 characters long'),
+});
